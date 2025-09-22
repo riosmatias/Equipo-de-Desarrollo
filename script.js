89b3c864c8a320369ee01a73b55391b8ece1dcd4
riosmatias <matiasgrios@gmail.com>
2025-09-21 23:47:29 -0300

    nuevos cambios

---
+++ script.js
@@ -0,0 +1,5 @@
+// Año dinámico en el footer
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// (Opcional) Podés interceptar clics aquí si más adelante querés lógica previa a la redirección
+// document.querySelectorAll('.btn').forEach(b=>b.addEventListener('click', (e)=>{ ... }));
